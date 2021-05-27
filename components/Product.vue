172789ae026028bd130232066d1f74b0d171f91a
Yahyagok <ygok17@hotmail.com>
2021-05-26 22:20:55 -0400

    added products

---
+++ components/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="item">
+    <img :src="img" />
+    <p>Name: {{ name }}</p>
+    <p>Price: {{ price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: ['name', 'img', 'price'],
+}
+</script>
+
+<style>
+.item {
+  flex: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,54 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">LimeLightCodingChallenge</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <Product
+      v-for="product in products"
+      :key="product.id"
+      :name="product.name"
+      :img="product.image"
+      :price="product.price"
+    />
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import Product from '../components/Product'
+
+export default {
+  components: {
+    Product,
+  },
+  data() {
+    return {
+      products: [],
+    }
+  },
+
+  async created() {
+    const config = {
+      headers: {
+        Accept: 'application/json',
+      },
+    }
+    try {
+      const res = await axios.get(
+        'https://trayvonnorthern.com/Edgewood-API/public/api/products',
+        config
+      )
+      this.products = res.data.data
+    } catch (err) {
+      console.log(err)
+    }
+  },
+}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
+/* Sample `apply` at-rules with Tailwind CSS */
+/* .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
+} */
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
